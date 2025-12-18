116b5d8ad3c49efdd00e1fc5051ac21dcbf9c703
24521320-ThuanPhat <24521320@gm.uit.edu.vn>
2025-12-18 13:45:08 +0700

    customer app

---
+++ LAB6/Customer App.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace LAB6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.dgvMenu = new System.Windows.Forms.DataGridView();
+            this.colID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colQuantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pnlBottom = new System.Windows.Forms.Panel();
+            this.btnOrder = new System.Windows.Forms.Button();
+            this.numTableID = new System.Windows.Forms.NumericUpDown();
+            this.lblTable = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMenu)).BeginInit();
+            this.pnlBottom.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numTableID)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(12, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(120, 35);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Kết nối Server";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // dgvMenu
+            // 
+            this.dgvMenu.AllowUserToAddRows = false;
+            this.dgvMenu.AllowUserToDeleteRows = false;
+            this.dgvMenu.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvMenu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMenu.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colID,
+            this.colName,
+            this.colPrice,
+            this.colQuantity});
+            this.dgvMenu.Location = new System.Drawing.Point(12, 60);
+            this.dgvMenu.Name = "dgvMenu";
+            this.dgvMenu.RowHeadersWidth = 51;
+            this.dgvMenu.RowTemplate.Height = 24;
+            this.dgvMenu.Size = new System.Drawing.Size(558, 250);
+            this.dgvMenu.TabIndex = 1;
+            // 
+            // colID
+            // 
+            this.colID.HeaderText = "ID Món";
+            this.colID.MinimumWidth = 6;
+            this.colID.Name = "colID";
+            this.colID.ReadOnly = true;
+            this.colID.Width = 80;
+            // 
+            // colName
+            // 
+            this.colName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colName.HeaderText = "Tên Món Ăn";
+            this.colName.MinimumWidth = 6;
+            this.colName.Name = "colName";
+            this.colName.ReadOnly = true;
+            // 
+            // colPrice
+            // 
+            this.colPrice.HeaderText = "Đơn Giá";
+            this.colPrice.MinimumWidth = 6;
+            this.colPrice.Name = "colPrice";
+            this.colPrice.ReadOnly = true;
+            this.colPrice.Width = 125;
+            // 
+            // colQuantity
+            // 
+            this.colQuantity.HeaderText = "Số Lượng Mua";
+            this.colQuantity.MinimumWidth = 6;
+            this.colQuantity.Name = "colQuantity";
+            this.colQuantity.Width = 125;
+            // 
+            // pnlBottom
+            // 
+            this.pnlBottom.Controls.Add(this.btnOrder);
+            this.pnlBottom.Controls.Add(this.numTableID);
+            this.pnlBottom.Controls.Add(this.lblTable);
+            this.pnlBottom.Location = new System.Drawing.Point(0, 320);
+            this.pnlBottom.Name = "pnlBottom";
+            this.pnlBottom.Size = new System.Drawing.Size(582, 83);
+            this.pnlBottom.TabIndex = 2;
+            // 
+            // btnOrder
+            // 
+            this.btnOrder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOrder.BackColor = System.Drawing.Color.LightGreen;
+            this.btnOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrder.Location = new System.Drawing.Point(400, 16);
+            this.btnOrder.Name = "btnOrder";
+            this.btnOrder.Size = new System.Drawing.Size(170, 50);
+            this.btnOrder.TabIndex = 2;
+            this.btnOrder.Text = "ĐẶT MÓN (ORDER)";
+            this.btnOrder.UseVisualStyleBackColor = false;
+            this.btnOrder.Click += new System.EventHandler(this.btnOrder_Click);
+            // 
+            // numTableID
+            // 
+            this.numTableID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numTableID.Location = new System.Drawing.Point(87, 28);
+            this.numTableID.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numTableID.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numTableID.Name = "numTableID";
+            this.numTableID.Size = new System.Drawing.Size(80, 26);
+            this.numTableID.TabIndex = 1;
+            this.numTableID.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // lblTable
+            // 
+            this.lblTable.AutoSize = true;
+            this.lblTable.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTable.Location = new System.Drawing.Point(12, 30);
+            this.lblTable.Name = "lblTable";
+            this.lblTable.Size = new System.Drawing.Size(66, 20);
+            this.lblTable.TabIndex = 0;
+            this.lblTable.Text = "Số bàn:";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.ForeColor = System.Drawing.Color.Red;
+            this.lblStatus.Location = new System.Drawing.Point(150, 21);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(89, 16);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Text = "Chưa kết nối...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 403);
+            this.Controls.Add(this.pnlBottom);
+            this.Controls.Add(this.dgvMenu);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Customer App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMenu)).EndInit();
+            this.pnlBottom.ResumeLayout(false);
+            this.pnlBottom.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numTableID)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.DataGridView dgvMenu;
+        private System.Windows.Forms.Panel pnlBottom;
+        private System.Windows.Forms.Button btnOrder;
+        private System.Windows.Forms.NumericUpDown numTableID;
+        private System.Windows.Forms.Label lblTable;
+
+        // Các cột của DataGridView
+        private System.Windows.Forms.DataGridViewTextBoxColumn colID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colPrice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colQuantity;
+    }
+}
---
+++ LAB6/Customer App.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnOrder.Enabled = false;
+            numTableID.Enabled = false;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Đang thử kết nối đến Server...", "Thông báo");
+            lblStatus.Text = "Đã kết nối";
+            lblStatus.ForeColor = Color.Green;
+            btnOrder.Enabled = true;
+            numTableID.Enabled = true;
+        }
+
+        private void btnOrder_Click(object sender, EventArgs e)
+        {
+            int tableID = (int)numTableID.Value;
+            MessageBox.Show($"Đã gửi yêu cầu đặt món cho bàn số {tableID}!", "Thành công");
+        }
+    }
+}
